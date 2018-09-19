9c4cbb698cbfa4b0fff3f2de104fb02a4810ffbe
Administrator <admin@localhost.localdomain>
2018-09-18 21:14:45 -0500

    changes from desktop: user modle, routes, and register view

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
 var app = express();
+
+// use sessions for tracking logins
+app.use(session({
+  secret: 'treehouse loves you',
+  resave: true,
+  saveUninitialized: false
+}));
+
+// mongoDB Connection
+mongoose.connect("mongodb://localhost:27017/bookworm", { useNewUrlParser: true });
+var db = mongoose.connection;
+
+// mongoDB error handler
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
--- models/user.js
+++ models/user.js
@@ -1,41 +1,31 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
-var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  name: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  favoriteBook: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
 
-});
+// create odject with form input});
+const userData = {
+  email: req.body.email,
+  name: req.body.name,
+  favoriteCar: req.body.favoriteCar,
+  password: req.body.password
+};
 
-// hash password before saving to database
-UserSchema.pre('save', function(next) {
-  var user = this;
-  bcrypt.hash(user.password, 10, function(err, hash) {
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  });
-});
 
-var User = mongoose.model('User', UserSchema);
-module.exports = User;
+// var UserSchema = new mongoose.Schema({
+//   //had user input fields in it, but not using Mongoose!
+//
+// });
+
+// // hash password before saving to database
+// UserSchema.pre('save', function(next) {
+//   var user = this;
+//   bcrypt.hash(user.password, 10, function(err, hash) {
+//     if (err) {
+//       return next(err);
+//     }
+//     user.password = hash;
+//     next();
+//   });
+// });
+
+// var User = mongoose.model('User', UserSchema);
+module.exports = userData;
